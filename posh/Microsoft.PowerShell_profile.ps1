464b3019fa4d41607e5ef9670091166446f05deb
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-04-21 15:36:51 +0200

    Added Powershell configuration files

---
+++ posh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "C:\Users\20204729\Documents\PowerShell\config.omp.json")
+Import-Module git-aliases -DisableNameChecking
+$DefaultUser = '20204729'
+
