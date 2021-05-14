0ca2d146ad0068e15e75e0767f6dbdb23d091490
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2021-05-14 14:37:39 +0200

    Fix cowsay and pip

--- scripts/install_cowsay.sh
+++ scripts/install_cowsay.sh
@@ -3,11 +3,15 @@
 set -euo pipefail
 
 # Download cowsay
+cd "${HOME}/Downloads"
 COWSAY_VERSION=v3.7.0
-curl -O -L https://github.com/cowsay-org/cowsay/archive/${COWSAY_VERSION}.tar.gz
-tar xf ${COWSAY_VERSION}.tar.gz
+curl -O -L "https://github.com/cowsay-org/cowsay/archive/${COWSAY_VERSION}.tar.gz"
+
+# Install cowsay
+tar xf "${COWSAY_VERSION}.tar.gz"
 mv cowsay-* cowsay
 cd cowsay
-make install prefix=$HOME/.local
-cd $HOME
-rm -r $HOME/Downloads/cowsay $HOME/Downloads/${COWSAY_VERSION}.tar.gz
+make install prefix="${HOME}/.local"
+
+# Clean up
+rm -r "${HOME}/Downloads/cowsay" "${HOME}/Downloads/${COWSAY_VERSION}.tar.gz"
