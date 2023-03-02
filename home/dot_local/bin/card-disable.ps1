d4ef93fe755c253259b7311787ac3412fed1b653
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2023-03-02 16:06:32 +0100

    Simplify card scripts

--- home/dot_local/bin/card-disable.ps1
+++ home/dot_local/bin/card-disable.ps1
@@ -8,21 +8,10 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 # Find the nvidia card
-$result = PNPUtil /enum-devices /class display | Select-Object -Skip 2 | Select-String -Pattern 'Manufacturer Name:' -Context 4, 2 | 
-Where-Object { $_.Line -replace '.*:\s+' -eq "NVIDIA" } | ForEach-Object {
-    [PSCustomObject]@{
-        InstanceId        = $PSItem.Context.PreContext[0] -replace '.*:\s+'
-        DeviceDescription = $PSItem.Context.PreContext[1] -replace '.*:\s+'
-        ClassName         = $PSItem.Context.PreContext[2] -replace '.*:\s+'
-        ClassGUID         = $PSItem.Context.PreContext[3] -replace '.*:\s+'
-        ManufacturerName  = $PSitem.Line -replace '.*:\s+'
-        Status            = $PSItem.Context.PostContext[0] -replace '.*:\s+'
-        DriverName        = $PSItem.Context.PostContext[1] -replace '.*:\s+'
-    }
-}
+$result = Get-PnpDevice -class Display -FriendlyName "NVIDIA*"
 
 if ($result) {
-    pnputil /disable-device $result.InstanceId
+    $result | Disable-PnpDevice -Confirm:$false
 } else {
     Write-Output "No NVIDIA card found."
     Start-Sleep 10
