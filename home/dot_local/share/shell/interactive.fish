e8bca5df409f21a8dc62dd4ab77ad9a5f4004726
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2025-09-15 11:40:20 +0200

    Update WSL detection

--- home/dot_local/share/shell/interactive.fish
+++ home/dot_local/share/shell/interactive.fish
@@ -2,7 +2,7 @@
 switch (uname -s) 
     case "Linux*"
         # Check if WSL
-        if cat /proc/version | grep -qi Microsoft;
+        if command -vq wslinfo;
             # We are in WSL, start gpg-relay agent
             source "$CUSTOM_SHELL_DIR/gpg-agent-relay2.fish"
         else
