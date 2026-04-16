7a569ec607e0d4bc3bdcd897a168713c4dc2a202
Joan Marcè i Igual <jmigual@users.noreply.github.com>
2026-04-16 15:25:43 +0200

    Fish shell load variables first

---
+++ home/dot_config/fish/conf.d/00_variables.fish
@@ -0,0 +1,3 @@
+set -x CUSTOM_SHELL_DIR "$HOME/.local/share/shell"
+source "$CUSTOM_SHELL_DIR/variables.fish"
+
--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,6 +1,5 @@
 # Source first thing to load the PATH
 set -x CUSTOM_SHELL_DIR "$HOME/.local/share/shell"
-source "$CUSTOM_SHELL_DIR/variables.fish"
 
 starship init fish | source
 
